a769d87a4fc43a5feca28f74957e0bbba45e4f2c
wupingyuan <wupingyuan2@h-partners.com>
2025-12-13 16:35:17 +0800

    del:删除没用的文件
    Signed-off-by: wupingyuan <wupingyuan2@h-partners.com>

--- Backup.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Backup.swift
-//  rive-react-native
-//
-//  Created by Peter G Hayes on 02/03/2023.
-//
-
-import Foundation
--- ios/RiveReactNative-Bridging-Header.h
+++
@@ -1,8 +0,0 @@
-#import <RiveRuntime/RiveRuntime.h>
-#import <React/RCTBridgeModule.h>
-#import <React/RCTViewManager.h>
-#import <React/RCTUIManager.h>
-#import <React/RCTUtils.h>
-#import <React/RCTExceptionsManager.h>
-#import <React/RCTView.h>
-#import <React/RCTEventEmitter.h>
